c12a8f97b5bbc9ba3dbbf85c16ee58006089d21b
Pritam Pradip Ghogare <preetam.ghogare@testingmavens.com>
2026-03-24 20:23:44 +0530

    Initial commit - EdWise App

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.title("Main App")
+
 if "redirected" not in st.session_state:
     st.session_state.redirected = True
     st.switch_page("pages/1_Student_Verification.py")
